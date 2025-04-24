723c8e6d15e02f7256bdd96f23a79c924dbe0d6d
istroberiless <kyle.inion@gmail.com>
2025-04-25 00:50:51 +0800

    cartmobile

--- mobile/config.ts
+++ mobile/config.ts
@@ -1,5 +1,5 @@
 // Update this URL to match your Spring Boot backend URL
-export const API_URL = 'http://192.168.1.10:8080';
+export const API_URL = 'http://192.168.111.240:8080';
 
 // Add other configuration constants here if needed
-export const AUTH_TOKEN_KEY = 'auth_token'; 
+export const AUTH_TOKEN_KEY = 'auth_token';
