0ac8dbe9c9d8694eae7827bcf7401bdc4016d50b
Carl Janssen Tampus <carljtampus@gmail.com>
2025-09-28 16:26:35 +0800

    initial plan 1

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://campus-eats-backend.onrender.com/api",
+    // baseURL: "http://localhost:8080/api", // for local testing
     headers: {
         "ngrok-skip-browser-warning": "true",
     },
