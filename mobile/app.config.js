d793cddcc18e35c5f3ce15bd601ec06afc51efd1
Carl Janssen Tampus <carljtampus@gmail.com>
2025-11-20 20:47:29 +0800

    please be good na tawn

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -87,7 +87,16 @@ export default ({ config }) => ({
       {
         locationAlwaysAndWhenInUsePermission: "Allow Campus Eats to use your location to find nearby restaurants and delivery options."
       }
-    ]
+    ],
+    [
+      "expo-camera",
+      {
+        cameraPermission: "Allow Campus Eats to access your camera for live streaming.",
+        microphonePermission: "Allow Campus Eats to access your microphone for live streaming.",
+        recordAudioAndroid: true
+      }
+    ],
+    "@config-plugins/react-native-webrtc"
   ],
   experiments: {
     typedRoutes: true
