5a172b46e8d95959db98d512814e1ab931259750
Kyle Niña Inion <kylenina.inion@cit.edu>
2025-11-26 16:48:07 +0800

    fix no show

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://campus-eats-backend.onrender.com/api",
-    //baseURL: "http://localhost:8080/api", // for local testing only
+    //baseURL: "https://campus-eats-backend.onrender.com/api",
+    baseURL: "http://localhost:8080/api", // for local testing only
     headers: {
         "ngrok-skip-browser-warning": "true",
     },
